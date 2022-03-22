306f1af2df009d28d2429ccc28d523619645565d
shpendnila <shpendnila7@gmail.com>
2022-03-22 21:44:07 +0100

    update web deployment to be created in the dev namespace

--- scripts/deploy-web-deployment.sh
+++ scripts/deploy-web-deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # create deployment
-kubectl create deployment web --image=gcr.io/google-samples/hello-app:1.0
+kubectl create deployment web --image=gcr.io/google-samples/hello-app:1.0 -n dev
 
 # expose deployment
-kubectl expose deployment web --type=ClusterIP --port=8080
+kubectl expose deployment web --type=ClusterIP --port=8080 -n dev
